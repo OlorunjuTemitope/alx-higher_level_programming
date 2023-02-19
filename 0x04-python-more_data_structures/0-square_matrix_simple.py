40e4c16a8676049e5014d74bc5e741832ca7b279
OlorunjuTemitope <Temitope>
2023-02-19 08:14:48 -0800

    Square

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new = []
-    for i in matrix:
-        new.append([j ** 2 for j in i])
-    return new
+    if not matrix:
+        print()
+    return [[item**2 for item in row] for row in matrix]
